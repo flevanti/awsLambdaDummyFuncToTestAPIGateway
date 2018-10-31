b8fb5646fed0cee495f567fd3471820b34be17a8
Francesco Levanti <francesco@levanti.net>
2018-10-31 21:33:40 +0000

    remove dummypayload file
    Not needed for deployment

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ rm -f main
 echo COMPILING FOR LINUX 🐧
 env GOOS=linux go build -o main *.go
 echo ZIPPING FOR AWS LAMBDA 👷️
-zip main.zip main dummyPayload.json .env
+zip main.zip main .env
 echo DONE 🍾
